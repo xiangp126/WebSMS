aa81de1cd13bc6bc843d5140de698d096e54ae35
corsair <xiangp126@126.com>
2018-04-17 06:21:37 +0000

    renew gapi for totally offline use

--- php/salary_draw.php
+++ php/salary_draw.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
         <title>税前薪水去向统计图</title>
-        <link href="google_api/tooltip.css" rel="stylesheet" type="text/css">
+        <link href="gapi/tooltip.css" rel="stylesheet" type="text/css">
     </head>
 
     <body style="font-family: Arial;border: 0 none;">
@@ -12,15 +12,17 @@
         <div id="visualization" style="width: 600px; height: 400px;"></div>
 
             <!-- load api -->
-        <script type="text/javascript" src="google_api/jsapi.js"></script>
+        <script type="text/javascript" src="gapi/jsapi.js"></script>
 
+        <!--
         <script type="text/javascript">
-            //load package
+            load package
             google.load('visualization', '1', {packages: ['corechart']});
         </script>
-        <script src="googl_api/corechart.js" type="text/javascript"></script>
-        <link href="google_api/ui+en.css" type="text/css" rel="stylesheet">
-        <script src="google_api/corechart+en.I.js" type="text/javascript"></script>
+        -->
+
+        <script type="text/javascript" src="gapi/jsapi.js" ></script>
+        <script type="text/javascript" src="gapi/uds_api_contents.js"></script>
 
         <script type="text/javascript">
             function drawVisualization() {
