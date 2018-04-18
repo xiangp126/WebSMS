bada6e26f8f671cdb67ccc7bc0b3d8636ee8cd30
corsair <xiangp126@126.com>
2018-04-18 08:53:01 +0000

    one choice for bg img

--- php/foot.php
+++ php/foot.php
@@ -1,7 +1,7 @@
 <body>
 	<div id="foot">
 		<div id="copyright" class='nav_bottom'>
-			@Copyright 苏州大学&nbsp;&nbsp;&nbsp; 宋月阳<br>
+			@Copyright by 苏州大学&nbsp;&nbsp;&nbsp; 宋月阳<br>
 		</div>
 	</div>
 </body>
--- php/indexx.php
+++ php/indexx.php
@@ -2,12 +2,9 @@
 require_once "login_check.php";
 require_once "head.php";
 ?>
-<div id="main" style="height:400px;">
+<div id="main">
     <div id="welcome">
         <div id = 'chinese'>欢迎登陆系统!</div>
-    </div>
-    <div id="img">
-        <img src="images/bz.png">
     </div>
 </div>
 <?php require_once "foot.php" ?>
