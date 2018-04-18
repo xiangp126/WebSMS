493ca4a9d14284860ddbbf5369be92598e60b005
corsair <xiangp126@126.com>
2018-04-18 07:52:16 +0000

    #3 new landspace

--- php/indexx.php
+++ php/indexx.php
@@ -2,8 +2,13 @@
 require_once "login_check.php";
 require_once "head.php";
 ?>
-<div id="main" style="height:204px;">
-    <div id="welcome"></div>
+<div id="main" style="height:400px;">
+    <div id="welcome">
+        <div id = 'chinese'>欢迎登陆系统!</div>
+    </div>
+    <div id="img">
+        <img src="images/bz.png">
+    </div>
 </div>
 <?php require_once "foot.php" ?>
 
